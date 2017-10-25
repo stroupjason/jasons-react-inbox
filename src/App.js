48cbcd3f2d2c8dfdb4319af9ddf208b9fb7c0fc1
Jason Stroup <jasonstroup12@gmail.com>
2017-10-25 02:14:00 -0600

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,21 +1,83 @@
+//import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { data } from './data'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+     class App extends Component {
+       render() {
+         console.log(data);
 
-export default App;
+       const messagesReadorUnread = (m) => {
+         const cls = ['row', 'message']
+       if (m.read) {
+           cls.push('read') } else {cls.push('unread') }
+         if (m.selected){
+           cls.push('selected')
+         }
+           return cls.join(' ')
+       }
+
+       const starred = (n) => {
+         const stars = ['start', 'fa']
+         if (n.starred){
+           stars.push('fa-star') } else { stars.push('fa-star-o') }
+           return stars.join(' ')
+       }
+     const check = (o) => {
+         if (o.selected) {
+            return (true) } else
+           { return (false) }
+       }
+       const starClick = data => {
+         if(data.starred){
+           data.starred=false
+         } else {
+           data.starred=true
+         }
+         this.setState(data)
+         }
+
+         const selectClick = data => {
+           if (data.selected){
+             data.selected=false
+           } else {
+           data.selected=true
+           }
+           this.setState(data)
+         }
+
+     var emails = data.map(function(data){
+         return <div key={data.id} className={messagesReadorUnread(data)}>
+                <div className="col-xs-1">
+                  <div className="row">
+                    <div className="col-xs-2">
+                      <input type="checkbox" onClick={selectClick.bind(this, data)} checked={!!check(data)}/>
+                    </div>
+                    <div className="col-xs-2">
+                      <i onClick={starClick.bind(this, data)} className={starred(data)}></i>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xs-11">
+                {(data.labels[0]) && <span className="label label-warning">{data.labels[0]}</span>}
+                {(data.labels[1]) && <span className="label label-warning">{data.labels[1]}</span>}
+                  <a href="#">
+                    {data.subject}
+                  </a>
+                </div>
+              </div>;
+
+      });
+
+
+         return (
+         <div>
+           {emails}
+           </div>
+             )
+       }
+     }
+
+
+ export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
